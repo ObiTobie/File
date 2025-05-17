d31846d5a3dc8c54523e4b41c834aa9e2b95a583
ObiTobie <hadesjus@gmail.com>
2025-05-17 16:07:19 +0700

    Update Attack.lua

--- Decompile/Attack.lua
+++ Decompile/Attack.lua
@@ -84,7 +84,7 @@ function CombatModule.StartCombat()
 		end
 
 		if comboIndex <= #unitConfig.Animations and tick() - lastAttackTime > unitConfig.TimePerAttack then
-			-- playAnimation(unitConfig.Animations[comboIndex])
+			playAnimation(unitConfig.Animations[comboIndex])
 
 			local skillUnit = game.ReplicatedFirst.SkillUnits:FindFirstChild(slotValue)
 			if skillUnit then
@@ -101,29 +101,29 @@ function CombatModule.StartCombat()
 			lastAttackTime = tick()
 			comboIndex += 1
 
-			if Player.Character.Shoot.Value then
-				CombatModule.BodyVelocityLook(Player.Character, -6, true)
-				Player.Character.UnlockCam.Value = true
-			else
-				CombatModule.BodyVelocityLook(Player.Character, 9)
-			end
+			-- if Player.Character.Shoot.Value then
+			-- 	CombatModule.BodyVelocityLook(Player.Character, -6, true)
+			-- 	Player.Character.UnlockCam.Value = true
+			-- else
+			-- 	CombatModule.BodyVelocityLook(Player.Character, 9)
+			-- end
 
-			local previousComboIndex = comboIndex
-			local humanoid = Player.Character.Humanoid
-			humanoid.AutoRotate = false
-			humanoid.WalkSpeed = 4
-			humanoid.JumpPower = 0
-			Player.Character.Skill.Value = true
+			-- local previousComboIndex = comboIndex
+			-- local humanoid = Player.Character.Humanoid
+			-- humanoid.AutoRotate = false
+			-- humanoid.WalkSpeed = 4
+			-- humanoid.JumpPower = 0
+			-- Player.Character.Skill.Value = true
 
-			task.wait(unitConfig.TimePerAttack)
+			-- task.wait(unitConfig.TimePerAttack)
 
-			Player.Character.Skill.Value = false
-			if comboIndex == previousComboIndex and Player.Character.Skill.Value == false then
-				humanoid.WalkSpeed = 25
-				humanoid.AutoRotate = true
-				humanoid.JumpPower = 50
-				Player.Character.UnlockCam.Value = false
-			end
+			-- Player.Character.Skill.Value = false
+			-- if comboIndex == previousComboIndex and Player.Character.Skill.Value == false then
+			-- 	humanoid.WalkSpeed = 25
+			-- 	humanoid.AutoRotate = true
+			-- 	humanoid.JumpPower = 50
+			-- 	Player.Character.UnlockCam.Value = false
+			-- end
 		end
 
 		if comboIndex > #unitConfig.Animations and tick() - lastAttackTime > 0 then
